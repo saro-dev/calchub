c39c8b04978a0a27f61b271943ef96b53c22621b
saro-dev <codersaro@gmail.com>
2024-05-03 09:37:33 +0530

    fixed bugs

--- age-calculator/assets/js/datepicker.js
+++ age-calculator/assets/js/datepicker.js
@@ -1,43 +1,36 @@
-document.querySelectorAll('.datepicker_us').forEach((datepicker) => {
-	new AirDatepicker(datepicker, {
-		dateFormat: 'MM/dd/yyyy',
-		autoClose: true,
-		onSelect: function(event){
-			return updateDate(event.datepicker.$el);
-		},
-		// visible: true,
-		prevHtml:
-			'<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 19L8 12L15 5" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-		nextHtml:
-			'<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 5L16 12L9 19" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-		locale: {
-			months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-			monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-			daysShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-			daysMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-		},
+$(function() {
+	$('.datepicker_us').datepicker({
+	  dateFormat: 'mm/dd/yy',
+	  prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 19L8 12L15 5" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	  nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 5L16 12L9 19" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	  onSelect: function(dateText, inst) {
+		updateDate(inst.input);
+		// Prevent the default behavior of scrolling and adding hash to URL
+		event.preventDefault();
+	  },
+	  beforeShow: function(input, inst) {
+		inst.dpDiv.addClass('datepicker-custom');
+	  },
+	  onClose: function(dateText, inst) {
+		inst.dpDiv.removeClass('datepicker-custom');
+	  }
 	});
-});
-
-document.querySelectorAll('.datepicker').forEach((datepicker) => {
-	new AirDatepicker(datepicker, {
-		dateFormat: 'yyyy-MM-dd',
-		autoClose: true,
-		onSelect: function(event){
-			return updateDate(event.datepicker.$el);
-		},
-		// visible: true,
-		prevHtml:
-			'<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 19L8 12L15 5" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-		nextHtml:
-			'<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 5L16 12L9 19" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-		locale: {
-			months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-			monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-			days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-			daysShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-			daysMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-		},
+  
+	$('.datepicker').datepicker({
+	  dateFormat: 'yy-mm-dd',
+	  prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 19L8 12L15 5" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	  nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 5L16 12L9 19" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	  onSelect: function(dateText, inst) {
+		updateDate(inst.input);
+		// Prevent the default behavior of scrolling and adding hash to URL
+		event.preventDefault();
+	  },
+	  beforeShow: function(input, inst) {
+		inst.dpDiv.addClass('datepicker-custom');
+	  },
+	  onClose: function(dateText, inst) {
+		inst.dpDiv.removeClass('datepicker-custom');
+	  }
 	});
-});
+  });
+  
